1edb4006b21d473f62b452505517512f4a084a87
ranggaoktavia409-maker <ranggaoktavia409@gmail.com>
2026-02-03 00:04:15 +0700

    Update script.js
    Signed-off-by: ranggaoktavia409-maker <ranggaoktavia409@gmail.com>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function orderFood(menu) {
-  const phone = "6281234567890"; // GANTI dengan nomor WhatsApp kamu
+  const phone = "62895352505993"; // GANTI dengan nomor WhatsApp kamu
   const message = `Halo, saya mau pesan ${menu}`;
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
   window.open(url, "_blank");
